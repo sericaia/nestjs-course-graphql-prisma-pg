996efbfdd67fa70a05511cf4ecc790d909fc7fc2
Daniela Matos de Carvalho <daniela.matosdecarvalho@maze.design>
2023-01-30 13:37:00 +0000

    feat: basic crud for coffees in gql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    CoffeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/coffees/coffees.resolver.ts
@@ -0,0 +1,28 @@
+import { ParseIntPipe } from '@nestjs/common';
+import { ID, Mutation, Query } from '@nestjs/graphql';
+import { Args, Resolver } from '@nestjs/graphql';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeInput } from './dto/create-coffee.input/create-coffee.input';
+import { Coffee } from './entities/coffee.entity/coffee.entity';
+
+@Resolver()
+export class CoffeesResolver {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+  @Query(() => [Coffee], { name: 'coffees' })
+  async findAll() {
+    return this.coffeesService.findAll();
+  }
+
+  @Query(() => Coffee, { name: 'coffee', nullable: true })
+  async findOne(@Args('id', { type: () => ID }, ParseIntPipe) id: number) {
+    return this.coffeesService.findOne(id);
+  }
+
+  @Mutation(() => Coffee, { name: 'createCoffee', nullable: true })
+  async create(
+    @Args('createCoffeeInput') createCoffeeInput: CreateCoffeeInput,
+  ) {
+    return this.coffeesService.create(createCoffeeInput);
+  }
+}
