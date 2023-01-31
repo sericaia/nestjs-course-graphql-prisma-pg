7f96765bdb802e8cccfc62f38d5a0d1d34e0bb3b
Daniela Matos de Carvalho <daniela.matosdecarvalho@maze.design>
2023-01-31 15:42:29 +0000

    feat: validations

--- src/coffees/dto/create-coffee.input.ts
+++ src/coffees/dto/create-coffee.input.ts
@@ -1,7 +1,9 @@
+import { MinLength } from 'class-validator';
 import { InputType } from '@nestjs/graphql';
 
 @InputType()
 export class CreateCoffeeInput {
+  @MinLength(3)
   name: string;
   brand: string;
   flavors: string[];
